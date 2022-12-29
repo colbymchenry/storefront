420e58f47c98e4a9ff90a09d09845ab981e2dbc6
Colby McHenry <me@colbymchenry.com>
2022-12-29 02:02:42 -0500

    cookies fix

--- src/lib/stores/cookies.ts
+++ src/lib/stores/cookies.ts
@@ -1,7 +1,7 @@
 import {type Writable, writable} from 'svelte/store';
 import type ICookie from "../interfaces/ICookie";
 
-export const {project_id} = JSON.parse(process.env.FIREBASE_CONFIG);
+export const project_id = typeof document !== 'undefined' ? JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)?.projectId : JSON.parse(process.env.FIREBASE_CONFIG)?.project_id;
 
 export const cookies: Writable<ICookie> = writable({
     is18: false,
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,7 +6,7 @@ import {project_id} from "../lib/stores/cookies";
 export const actions = {
     // @ts-ignore
     enter: async ({request, cookies}) => {
-        const cookie: ICookie = JSON.parse(cookies.get(project_id));
+        const cookie: ICookie = JSON.parse(cookies.get(project_id) || "{}");
         cookie.is18 = true;
         cookies.set(project_id, JSON.stringify(cookie));
         throw redirect(303, '/');
@@ -15,7 +15,7 @@ export const actions = {
     authenticate: async ({request, cookies}) => {
         let cookie: ICookie;
         try {
-            cookie = JSON.parse(cookies.get(project_id));
+            cookie = JSON.parse(cookies.get(project_id) || "{}");
         } catch (error) {
             cookie = {
                 authenticated: false,
