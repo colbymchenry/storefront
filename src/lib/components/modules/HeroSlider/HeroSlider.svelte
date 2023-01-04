d09e997d6cabdc79d2ec808baafde6dc3e8865f7
Colby McHenry <me@colbymchenry.com>
2023-01-04 14:39:25 -0500

    Hero slider completed

--- src/lib/components/modules/HeroSlider/HeroSlider.svelte
+++ src/lib/components/modules/HeroSlider/HeroSlider.svelte
@@ -19,26 +19,31 @@
 
 <div class="w-full">
     <Component {schema} let:props>
-        <div class={`flex relative bg-black overflow-hidden`} style={!props.getBlocks("Slide").filter(useFilter).length ? 'min-height: 20vh;': ''}>
-            <div class={`flex flex-nowrap w-full slider-container`} style={`margin-left: -${100 * slide}%;`}>
+        <div class={`flex relative bg-black overflow-hidden`}
+             style={!props.getBlocks("Slide").filter(useFilter).length ? 'min-height: 20vh;': ''}>
+            <div class={`flex flex-nowrap w-full slider-container z-10`} style={`margin-left: -${100 * slide}%;`}>
                 {#each props.getBlocks("Slide").filter(useFilter) as block, index}
-                    <img src={block.image} loading="lazy" alt={props.alt}/>
+                    <a href={block.url || '#'} class="min-w-full h-full">
+                        <img src={block.image} loading="lazy" alt={props.alt}/>
+                    </a>
                 {/each}
             </div>
 
-            <div class={`absolute top-0 left-0 w-full h-full grid grid-rows-3 z-10 p-1 md:p-3 text-${props.controlsColor}`}>
+            <div class={`absolute top-0 left-0 w-full h-full grid grid-rows-3 p-1 md:p-3 text-${props.controlsColor}`}>
                 <div></div>
                 <div class="flex justify-between align-center w-full h-full">
-                    <button type="button" class="opacity-80 hover:opacity-100 transition" on:click={() => handleBack(props)}>
+                    <button type="button" class="opacity-80 hover:opacity-100 transition z-10"
+                            on:click={() => handleBack(props)}>
                         <span class="flex items-center justify-center material-symbols-outlined text-sm md:text-lg pointer-events-none">arrow_back_ios</span>
                     </button>
-                    <button type="button" class="opacity-80 hover:opacity-100 transition" on:click={() => handleNext(props)}>
+                    <button type="button" class="opacity-80 hover:opacity-100 transition z-10"
+                            on:click={() => handleNext(props)}>
                         <span class="flex items-center justify-center material-symbols-outlined text-sm md:text-lg pointer-events-none">arrow_forward_ios</span>
                     </button>
                 </div>
                 <div class="flex justify-center items-end h-full">
                     {#each props.getBlocks("Slide").filter(useFilter) as block, index}
-                        <button type="button" class="hover:opacity-100 transition" on:click={() => slide = index}
+                        <button type="button" class="hover:opacity-100 transition z-10" on:click={() => slide = index}
                                 class:opacity-100={slide === index} class:opacity-80={slide !== index}>
                             <span class="material-symbols-outlined mx-1 shadow-2xl text-sm md:text-lg pointer-events-none">
                                 {#if slide === index}
