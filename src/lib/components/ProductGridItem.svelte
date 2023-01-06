4a08ca1080069582eeb00de0a459a20387150d15
Colby McHenry <me@colbymchenry.com>
2023-01-06 17:07:35 -0500

    ProductGridItem

---
+++ src/lib/components/ProductGridItem.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    export let product: any = undefined;
+    export let props: any = undefined;
+
+</script>
+
+<a href={`/product/${product.id}`} class="product">
+    <img src={product.hdThumbnailUrl} loading="lazy" class={`h-${props?.imgHeight}`}/>
+    <span><strong>{product.name}</strong></span>
+    {#if product?.attributes?.length && product.attributes.find((att) => att.type === "UPC")}
+        <small>UPC: {product.attributes.find((att) => att.type === "UPC").value}</small>
+    {/if}
+</a>
+
+<style lang="scss">
+  .product {
+    @apply flex flex-col max-h-full relative;
+    text-align: center;
+    width: 20vh;
+    min-width: 20vh;
+
+    span {
+      @apply text-xs md:text-sm mt-4 flex-grow;
+      font-family: 'IBM Plex Sans', sans-serif;
+    }
+
+    small {
+      @apply mt-2;
+    }
+
+    img {
+      width: auto;
+      object-fit: contain;
+    }
+  }
+</style>
--- src/lib/components/modules/FeaturedCollection/FeaturedCollection.svelte
+++ src/lib/components/modules/FeaturedCollection/FeaturedCollection.svelte
@@ -3,7 +3,7 @@
     import Component from "$lib/components/component/Component.svelte";
     import {lightspeedClientUtils} from "$lib/utils/lightspeed-utils";
     import {store} from "$lib/stores/store.js";
-    import {objectHelper} from "$lib/utils/object-helper.js";
+    import ProductGridItem from "$lib/components/ProductGridItem.svelte";
 
     export let key: string = undefined;
 
@@ -44,13 +44,7 @@
             {:else if products?.length}
                 <div class="products">
                     {#each products as product}
-                        <a href={`/product/${product.id}`} class="product">
-                            <img src={product.hdThumbnailUrl} loading="lazy" class={`h-${props.imgHeight}`}/>
-                            <span><strong>{product.name}</strong></span>
-                            {#if product?.attributes?.length && product.attributes.find((att) => att.type === "UPC")}
-                                <small>UPC: {product.attributes.find((att) => att.type === "UPC").value}</small>
-                            {/if}
-                        </a>
+                        <ProductGridItem {product} {props} />
                     {/each}
                 </div>
             {/if}
@@ -70,27 +64,6 @@
 
   .products {
     @apply overflow-x-auto overflow-y-hidden max-h-full relative flex flex-nowrap gap-10;
-  }
-
-  .product {
-    @apply flex flex-col max-h-full relative;
-    text-align: center;
-    width: 20vh;
-    min-width: 20vh;
-
-    span {
-      @apply text-xs md:text-sm mt-4 flex-grow;
-      font-family: 'IBM Plex Sans', sans-serif;
-    }
-
-    small {
-      @apply mt-2;
-    }
-
-    img {
-      width: auto;
-      object-fit: contain;
-    }
   }
 </style>
 
