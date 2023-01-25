b870150ad6d5501621f7b05135a17de39447b97b
Colby McHenry <me@colbymchenry.com>
2023-01-24 20:37:30 -0500

    Working on auth modal

---
+++ src/lib/components/AuthModal/AuthModal.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Modal from "$lib/components/Modal.svelte";
+
+    export let visible = true;
+</script>
+
+<Modal bind:visible>
+    <form class="w-full flex flex-col">
+
+    </form>
+</Modal>
